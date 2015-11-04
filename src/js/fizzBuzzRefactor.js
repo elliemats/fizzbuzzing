dbe6eac38ce8b0c14f26eb21c6e5b0bed622b135
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-11-03 19:11:28 -0600

    Add refactored fizzbuzz js solution.

---
+++ src/js/fizzBuzzRefactor.js
@@ -0,0 +1,27 @@
+/* ORIGINAL FIZZBUZZ SOLUTION */
+
+for (var i=1; i <= 20; i++)
+{
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
+}
+
+/* REFACTORED SOLUTION */
+
+function myFizzBuzz (n) {
+  if (typeof n !== 'number') {
+    return 'Input must be an integer.';
+  }
+  for (var i = 1; i <= n; i++) {
+    var output = '';
+    if (i % 3 === 0) output += 'Fizz';
+    if (i % 5 === 0) output += 'Buzz';
+    console.log(output || i);
+  }
+}
