a4b3e219cf5589497332ff84e5dac410f14d9d7a
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-11-04 11:35:30 -0600

    Add additional test to test integer output.

--- src/specs/fizzBuzzRefactor.spec.js
+++ src/specs/fizzBuzzRefactor.spec.js
@@ -8,22 +8,27 @@
 
       var result;
 
-      it('Return "Fizz" for any multiple of 3', function () {
+      it('should return "Fizz" for any multiple of 3', function () {
           result = fizzBuzzIt('peanut');
           expect(result).toEqual("Input must be an integer.");
       });
 
-      it('Return "Fizz" for any multiple of 3', function () {
+      it('should return "Fizz" for any multiple of 3', function () {
           result = fizzBuzzIt(5);
           expect(result[2]).toEqual("Fizz");
       });
 
-      it('Return "Buzz" for any multiple of 3', function () {
+      it('should return a number for any value that is not divisible by 3 or 5.', function () {
+          result = fizzBuzzIt(1);
+          expect(result[0]).toEqual(1);
+      });
+
+      it('should return "Buzz" for any multiple of 3', function () {
           result = fizzBuzzIt(5);
           expect(result[4]).toEqual("Buzz");
       });
 
-      it('Return "FizzBuzz" for any multiple of 3', function () {
+      it('should return "FizzBuzz" for any multiple of 3', function () {
           result = fizzBuzzIt(15);
           expect(result[14]).toEqual("FizzBuzz");
       });
